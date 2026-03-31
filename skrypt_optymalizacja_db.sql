3b77e05a710e1731f9630c41ed54f506673da6d6
Mateusz Marciniuk <94458663+Mateusz-Marciniuk@users.noreply.github.com>
2026-03-31 17:05:10 +0200

    Simplify database script by removing data insertion
    Removed the procedure for inserting demo data and kept only the database and table creation.

--- skrypt_optymalizacja_db.sql
+++ skrypt_optymalizacja_db.sql
@@ -10,19 +10,3 @@ CREATE TABLE pracownicy (
     pensja DECIMAL(10,2),
     data_zatrudnienia DATE
 );
-
--- Wstaw 10000 rekordów (demo z dużą ilością danych)
-DELIMITER //
-CREATE PROCEDURE WstawDane()
-BEGIN
-    DECLARE i INT DEFAULT 1;
-    WHILE i <= 10000 DO
-        INSERT INTO pracownicy (imie, nazwisko, departament, pensja, data_zatrudnienia)
-        VALUES (CONCAT('Imie', i), CONCAT('Nazwisko', i), CONCAT('Dept', MOD(i,10)+1), RAND()*10000+2000, DATE_SUB(NOW(), INTERVAL FLOOR(RAND()*3650) DAY));
-        SET i = i + 1;
-    END WHILE;
-END //
-DELIMITER ;
-
-CALL WstawDane();
-DROP PROCEDURE WstawDane;
